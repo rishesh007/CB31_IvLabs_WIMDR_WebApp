063f8fd25cefe829958c16dacedac7cf1471dcfe
rishesh007 <risheshagarwala1@gmail.com>
2020-07-13 13:46:24 +0530

    Settings Screen Added

---
+++ lib/screens/main_screen/settings/settings.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingsView extends StatefulWidget {
+  @override
+  _SettingsViewState createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.red,
+    );
+  }
+}
