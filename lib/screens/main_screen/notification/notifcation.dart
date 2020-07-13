a07337b6fa315dc67ce83ed324e458995265dc77
rishesh007 <risheshagarwala1@gmail.com>
2020-07-13 13:46:04 +0530

    Notification Screen Added

---
+++ lib/screens/main_screen/notification/notifcation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class NotificationView extends StatefulWidget {
+  @override
+  _NotificationViewState createState() => _NotificationViewState();
+}
+
+class _NotificationViewState extends State<NotificationView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.yellow,
+    );
+  }
+}
