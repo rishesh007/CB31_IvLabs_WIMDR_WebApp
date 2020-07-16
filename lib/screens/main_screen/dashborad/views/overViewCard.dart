667848dc04d29748545c9ce49a5d70e5c2237f3d
rishesh007 <risheshagarwala1@gmail.com>
2020-07-16 15:03:16 +0530

    Overview Layout Rectified

--- lib/screens/main_screen/dashborad/views/overViewCard.dart
+++ lib/screens/main_screen/dashborad/views/overViewCard.dart
@@ -1,3 +1,5 @@
+import 'package:falcon_vision/models/overview.dart';
+import 'package:falcon_vision/widgets/custom_drop_down/view_type/type_dropdown.dart';
 import 'package:flutter/material.dart';
 
 class OverViewCard extends StatefulWidget {
@@ -7,207 +9,149 @@ class OverViewCard extends StatefulWidget {
 
 class _OverViewCardState extends State<OverViewCard> {
   String dropdownValue = 'Today';
+
+  void changeTypeData(num val) {
+    setState(() {
+      currentEnabledData = val;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    
     return Container(
       // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       height: 300,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Card(
-          elevation: 5,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 8, 10, 16),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Overview',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                      child: Container(
-                        width: 130.0,
-                        child: DropdownButton<String>(
-                          value: dropdownValue,
-                          icon: Icon(
-                            Icons.arrow_downward,
-                            color: Colors.blueGrey[800],
-                          ),
-                          iconSize: 24,
-                          elevation: 16,
-                          style: TextStyle(
-                              color: Colors.blueGrey[800], fontSize: 20),
-                          underline: Container(
-                            height: 0,
-                            color: Colors.blueGrey[800],
-                          ),
-                          onChanged: (String newValue) {
-                            setState(() {
-                              dropdownValue = newValue;
-                            });
-                          },
-                          items: <String>['Today', 'Last Week', 'Last Month']
-                              .map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(
-                                value,
-                                style: TextStyle(
-                                  color: Colors.blueGrey[800],
-                                ),
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20.0, 0, 20, 20),
-                  child: Row(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 8, 10, 16),
+              child: Column(
+                children: <Widget>[
+                  Row(
                     children: [
-                      Expanded(
-                        flex: 1,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            Row(
-                              // mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  'Enteries ',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.blueGrey,
-                                  ),
-                                ),
-                                Text(
-                                  '+234',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.red[900],
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 5.0,
-                            ),
-                            Align(
-                              alignment: Alignment.bottomLeft,
-                              child: Text(
-                                '2087',
-                                style: TextStyle(
-                                  fontSize: 35,
-                                  color: Colors.blueGrey[600],
-                                ),
-                              ),
-                            ),
-                          ],
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: SelectableText(
+                          'Overview',
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
                       Expanded(
-                        flex: 1,
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  'Exit ',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.blueGrey,
-                                  ),
-                                ),
-                                Text(
-                                  '-98',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.red[900],
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 5.0,
-                            ),
-                            Text(
-                              '2074',
-                              style: TextStyle(
-                                fontSize: 35,
-                                color: Colors.blueGrey[600],
-                              ),
-                            )
-                          ],
-                        ),
+                        child: Container(),
                       ),
-                      Expanded(
-                        flex: 1,
-                        child: Column(
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                Text(
-                                  'Total ',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.blueGrey,
-                                  ),
-                                ),
-                                Text(
-                                  '+145',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.red[900],
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 5.0,
-                            ),
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: Text(
-                                '3184',
-                                style: TextStyle(
-                                  fontSize: 35,
-                                  color: Colors.blueGrey[600],
-                                ),
-                              ),
-                            )
-                          ],
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                        child: Container(
+                          width: 220,
+                          child: TypeDropDown(
+                            currentType: currentEnabledData,
+                            changeTypeData: changeTypeData,
+                          ),
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    // color: Colors.green,
+                    // width: 700,
+                    height: 150,
+                    child: DataWidget(
+                      dataSeq: currentEnabledData,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class DataWidget extends StatefulWidget {
+  final num dataSeq;
+  DataWidget({@required this.dataSeq});
+
+  @override
+  _DataWidgetState createState() => _DataWidgetState();
+}
+
+class _DataWidgetState extends State<DataWidget> {
+  Widget dataBox(text, incData, tData) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            SelectableText(
+              text,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.blueGrey,
+              ),
+            ),
+            SelectableText(
+              (incData < 0) ? incData.toString() : '+' + incData.toString(),
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.red[900],
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        SelectableText(
+          tData.toString(),
+          style: TextStyle(
+            fontSize: 35,
+            color: Colors.blueGrey[600],
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        dataBox(
+          'Entry ',
+          overviewData[widget.dataSeq].incEntry,
+          overviewData[widget.dataSeq].entry,
+        ),
+        Expanded(child: Container()),
+        dataBox(
+          'Exit ',
+          overviewData[widget.dataSeq].incExit,
+          overviewData[widget.dataSeq].exit,
+        ),
+        Expanded(child: Container()),
+        dataBox(
+          'Total ',
+          overviewData[widget.dataSeq].incTotal,
+          overviewData[widget.dataSeq].total,
+        ),
+      ],
     );
   }
 }
