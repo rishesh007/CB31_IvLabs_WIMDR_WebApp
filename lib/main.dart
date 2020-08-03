64448cce57f7f10a627a0f9c1b8c34191048a945
rishesh007 <risheshagarwala1@gmail.com>
2020-08-03 16:53:56 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
         '/register': (context) => RegisterWrapper(),
         '/user': (context) => MainScreen(),
       },
-      // localeResolutionCallback: ,
       theme: ThemeData(
         primaryColor: Colors.blue,
         fontFamily: 'archiaregular',
